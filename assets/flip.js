4bde7572a0e79d4d0e0a903d0914641d4003c145
ina <siddi352@newschool.edu>
2023-04-04 21:29:12 -0400

    FIXED clock and js of flip

--- assets/flip.js
+++ assets/flip.js
@@ -1,7 +1,39 @@
+window.stateCallback = () => {
+	console.log('Something changed!')
+}
+
+  let status=[0,0,0,0,0,0,0,0,0]
+  // let statusStorage= localStorage.getItem("status")
+  // ? JSON.parse(localStorage.getItem("status"))
+  //   : status; 
+
+
+
 var cards = document.querySelectorAll('.card');
 
 [...cards].forEach((card)=>{
   card.addEventListener( 'click', function() {
-    card.classList.toggle('is-flipped');
+    console.log(card,card.classList,card.innerHTML)
+
+
+    // card.classList.toggle('is-flipped');
+
+    if(card.classList.contains('is-flipped')){
+      card.classList.remove('is-flipped')
+     var innervalue= card.querySelector('.card__face--front').innerHTML 
+    var itemId=parseInt(card.id.replace('card_',''))
+    status[itemId]=0
+     console.log(status)
+     
+    }
+
+    
+    else{card.classList.add('is-flipped')
+    var innervalue= card.querySelector('.card__face--back').innerHTML 
+    var itemId=parseInt(card.id.replace('card_',''))
+    status[itemId]=1
+    console.log(status)
+  }
+
   });
 });
