b8f6759d3d370ecb8fcbfa84e29467be13ab2888
ina <siddi352@newschool.edu>
2023-04-17 16:43:17 -0400

    added bg color changer

---
+++ assets/bg.js
@@ -0,0 +1,5 @@
+$('.patch').click(function(){
+    $(this).addClass('active');
+    $('.patch').not($(this)).removeClass('active');
+    $('body').attr('class',$(this).attr('class').split(' ')[1]);
+});
