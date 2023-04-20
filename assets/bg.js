7e44d0c6b7bc4f69be5bdd260048bc23f78def78
ina <siddi352@newschool.edu>
2023-04-20 18:20:01 -0400

    mobile layout edits

--- assets/bg.js
+++ assets/bg.js
@@ -1,13 +1,17 @@
 
 const modeBtn = document.getElementById('mode');
+const text = document.getElementById('lightmode');
+
 modeBtn.onchange = (e) => {
   if (modeBtn.checked === true) {
     document.documentElement.classList.remove("light")
     document.documentElement.classList.add("dark")
+    text.innerHTML = 'Dark Mode';
     window.localStorage.setItem('mode', 'dark');
   } else {
     document.documentElement.classList.remove("dark")
     document.documentElement.classList.add("light")
+    text.innerHTML = 'Light Mode';
     window.localStorage.setItem('mode', 'light');
   }
 }
@@ -17,10 +21,12 @@ if (mode == 'dark') {
   modeBtn.checked = true;
   document.documentElement.classList.remove("light")
   document.documentElement.classList.add("dark")
+  text.innerHTML = 'Dark Mode';
 }
 
 if (mode == 'light') {
   modeBtn.checked = false;
   document.documentElement.classList.remove("dark")
   document.documentElement.classList.add("light")
+  text.innerHTML = 'Light Mode';
 }
